4c5ca2da5db1bac3d968e3b39c012858235599b7
Julian <parrishjulian62@gmail.com>
2017-09-04 21:42:07 -0400

    More settling on external library loading

--- src/Components/Images.js
+++ src/Components/Images.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
-import $ from 'jquery'
-import ReactDOM from 'react-dom'
+import $ from 'jquery';
+
 class Images extends Component {
 
   componentDidMount(){
